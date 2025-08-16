f561b08dbc2c0bb5c67fbbb6d6bc2422ee10c7c2
Jakkapat2548 <dreammm999d@gmail.com>
2025-08-16 10:40:52 +0700

    0002

--- Takiang2.0/src/Page/Login.jsx
+++ Takiang2.0/src/Page/Login.jsx
@@ -32,7 +32,7 @@ function Login() {
     }
 
     try {
-      const res = await axios.post('http://localhost:3001/api/login', formData)
+       const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/login`, formData)
       const { message, team, user_id, username } = res.data  // <-- เพิ่ม username
 
       // บันทึก team, user_id, username ลง localStorage
